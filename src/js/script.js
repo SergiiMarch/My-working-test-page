792962f4fc9b53671cc8062136338141059a9c53
Sergii Marchenko <marikmarchelo@gmail.com>
2024-07-15 08:03:18 +0300

    add commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,20 @@
 const form = document.querySelector('.search-js');
-console.dir(form);
+const divEl = document.querySelector('.random-js');
+
+const API_URL = 'https://swapi.dev/api/';
+
+fetch(API_URL)
+  .then(res => {
+    if (!res.ok) {
+      throw new Error(res.status);
+    }
+    return res.json();
+  })
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+}
